3fbebc5f3311a56e8db29b37f35986668ad49227
Andrew Harris <adharris@gmail.com>
2017-04-21 19:35:36 -0400

    Problem 13

--- problems/problems_000_099/problems_010_019/__init__.py
+++ problems/problems_000_099/problems_010_019/__init__.py
@@ -1,9 +1,11 @@
 
+from .problem_013 import problem_013
 from .problem_012 import problem_012
 from .problem_010 import problem_010
 from .problem_011 import problem_011
 
 all_commands = [
+    problem_013,
     problem_012,
     problem_010,
     problem_011,
